9ce3f3d500b23121a4ccf2eeb1137bdeeef551bb
David MAC <david@wbcboxing.com>
2023-01-20 13:41:21 -0600

    agregando el cierre de sesion

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -24,5 +25,7 @@ Route::post('/crear-cuenta', [RegisterController::class,'store']);
 
 Route::get('/login',[LoginController::class,'index'])->name('login');
 Route::post('/login',[LoginController::class,'store']);//--> se omite name ya que toma el anterior
+
+Route::get('/logout',[LogoutController::class,'store'])->name('logout');
 
 Route::get('/muro',[PostController::class,'index'])->name('posts.index');
