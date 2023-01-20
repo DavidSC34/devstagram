834068c3137bb477a00e5fdbd09f7f61543f6b23
David MAC <david@wbcboxing.com>
2023-01-20 16:00:34 -0600

    Como mantener la sesion abierta

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -14,13 +14,14 @@ class LoginController extends Controller
     }
     public function store(Request $request)
     {
-        // dd('Autenticando... ');
+        //  dd($request->remember);
         $this->validate($request,[
             'email'=>'required|email',
             'password'=>'required',
         ]);
-
-        if(!auth()->attempt($request->only('email','password'))){
+      
+           
+        if(!auth()->attempt($request->only('email','password'), $request->remember)){
             return back()->with('mensaje','Credenciales incorrectas');
         }
 
