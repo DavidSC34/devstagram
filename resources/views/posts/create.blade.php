8e863a8386551505be91a72686e71e23c51bc716
David MAC <david@wbcboxing.com>
2023-01-25 12:52:35 -0600

    terminando el form de crear publicacion

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -15,17 +15,25 @@ Crea una nueva Publicacion
                     @csrf
                     <div class="mb-5">
     
-                        <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">Nombre</label>
-                        <input type="text" id="name" name="name" placeholder="Tu nombre" class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror" value={{old('name')}}>
+                        <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">Titulo</label>
+                        <input type="text" id="titulo" name="titulo" placeholder="Titulo de la publicacion" class="border p-3 w-full rounded-lg @error('titulo') border-red-500 @enderror" value={{old('titulo')}}>
                     </div>
-                    @error('name')  
+                    @error('titulo')  
+                        <p class="bg-red-500 text-white my-2 text-sm p-2 text-center rounded-lg">{{ $message }}</p>
+                    @enderror
+                    <div class="mb-5">
+    
+                        <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">Descripcion</label>
+                        <textarea  id="descripcion" name="descripcion" placeholder="Descripcion de la publicacion" class="border p-3 w-full rounded-lg @error('descripcion') border-red-500 @enderror" >{{old('descripcion')}}</textarea>
+                    </div>
+                    @error('descripcion')  
                         <p class="bg-red-500 text-white my-2 text-sm p-2 text-center rounded-lg">{{ $message }}</p>
                     @enderror
 
                    
                  
     
-                    <input type="submit" value="Crear cuenta" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+                    <input type="submit" value="Crear publicacion" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
                 </form>
             </div>
     </div>
