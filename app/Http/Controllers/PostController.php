8dfa15e6fc25f53575a1e929ee2729f42e15e604
David MAC <david@wbcboxing.com>
2023-01-30 11:57:38 -0600

    Creandio los posts y ademas, validando la imagen en el post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -32,6 +33,15 @@ class PostController extends Controller
             'descripcion'=> 'required',
             'imagen'=> 'required',
            ]);
+
+           Post::create([
+                'titulo'=> $request->titulo,
+                'descripcion'=> $request->descripcion,
+                'imagen'=> $request->imagen,
+                'user_id'=> auth()->user()->id
+           ]);
+
+           return redirect()->route('posts.index', auth()->user()->username);
     }
 
    
