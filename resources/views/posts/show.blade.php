08f7d14730c2110a899cad0aca00cec67a5e1578
David MAC <david@wbcboxing.com>
2023-02-03 17:02:31 -0600

    uso de if, clciulo orcwch''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -23,7 +23,11 @@
             </div>
         </div>
         <div class="md:w-1/2 p-5">
-            <div class="shad    ow bg-white p-5 mb-5">
+            <div class="shadow bg-white p-5 mb-5">
+
+                @auth
+                    
+               
                 <p class="text-xl font-bold text-center mb-4"> Agrega un Nuevo comentario</p>
                 @if(session('mensaje'))
                     <div class="bg-green-500 p-2 rounded-lg mb-6 text-white text-center uppercase font-bold">
@@ -42,6 +46,21 @@
                     </div>
                     <input type="submit" value="Comentar" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
                 </form>
+            @endauth
+
+                    <div class="bg-white shadow mb-5 max-h-96 overflow-y-scroll mt-10">
+                      @if ($post->comentarios->count())
+                          @foreach ($post->comentarios as $comentario)
+                          <div class="p-5 border-gray-300 border-b">
+                            <a href="{{ route('posts.index',$comentario->user)}}" class="font-bold">{{$comentario->user->username}}</a>
+                              <p>{{ $comentario->comentario}}</p>
+                              <p class="text-sm text-gray-500">{{ $comentario->created_at->diffForHumans()}}</p>
+                          </div>
+                          @endforeach
+                      @else
+                          <p class="p-10 text-center">No hay comentarios aun</p>
+                      @endif
+                    </div>
             </div>
         </div>
 </div>
