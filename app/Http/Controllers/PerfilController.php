6c224a86169cb292ef4e77fbe092c7e9777aa22d
David MAC <david@wbcboxing.com>
2023-02-10 17:37:08 -0600

    validar form y mas reglas para editar perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PerfilController extends Controller
@@ -17,5 +18,15 @@ class PerfilController extends Controller
     {
         // dd('Aqui se muestra el perfil del usuario');
         return view('perfil.index');
+    }
+
+    public function store(Request $request)
+    {
+        // dd('guardando cambios ');
+        //Modificar el request (para evitar el username repetido)
+        $request->request->add(['username'=>Str::slug($request->username)]);
+        $this->validate($request,[
+            'username'=>['required','unique:users','min:3','max:20','not_in:twitter,editar-perfil'],//--> cuando son mas de tres reglas se ponen en array
+        ]);
     }
 }
