9cdce1514e69e6e7127a0a16d73201d901590e72
David MAC <david@wbcboxing.com>
2023-02-11 17:45:09 -0600

    mostrar imagen de perfil con un ternario en caso de no tener y ademas mostrar total de post del usuario

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,11 @@
 <div class="flex justify-center mt-5">
     <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
             <div class="w-8/12 lg:w-6/12 px-5">
-                <img src="{{asset('img/usuario.svg')}}" alt="imagen usuario">
+                <img 
+                    src="{{ $user->imagen ? asset('perfiles').'/'.$user->imagen : 
+                    asset('img/usuario.svg')}}" 
+                    alt="imagen usuario"
+                    >
             </div>
             <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py-10 md:py-10">
                 {{-- {{dd($user)}} --}}
@@ -36,7 +40,7 @@
                     <span class="font-normal">Siguiendo</span>
                 </p>
                 <p class="text-gray-800 text-sm mb-3 font-bold">
-                    0
+                    {{$user->posts->count()}}
                     <span class="font-normal">Post</span>
                 </p>
             </div>
