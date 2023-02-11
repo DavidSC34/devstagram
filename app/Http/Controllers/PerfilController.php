eefb17ab6f74a25b792180dcdea0dad37a8fe356
David MAC <david@wbcboxing.com>
2023-02-11 17:09:17 -0600

    creando migracion para agrergar imagen, y ademas  el detalle si hay imagen en base de datos no sobreescribir

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PerfilController extends Controller
 {
@@ -26,7 +28,31 @@ class PerfilController extends Controller
         //Modificar el request (para evitar el username repetido)
         $request->request->add(['username'=>Str::slug($request->username)]);
         $this->validate($request,[
-            'username'=>['required','unique:users','min:3','max:20','not_in:twitter,editar-perfil'],//--> cuando son mas de tres reglas se ponen en array
+            'username'=>['required','unique:users,username,'.auth()->user()->id,'min:3','max:20','not_in:twitter,editar-perfil'],//--> cuando son mas de tres reglas se ponen en array
         ]);
+
+        if($request->imagen){
+            $imagen = $request->file('imagen');
+            //modificar imagenes
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();//-->genera id unico
+            $imagenServidor = Image::make($imagen);
+            $imagenServidor->fit(1000,1000);
+
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+            $imagenServidor->save($imagenPath);
+           
+        } 
+        //Guardar cambios
+        $usuario = User::find( auth()->user()->id);
+
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? null; //--> no la elimina pero si hay img en base de datos no la borra
+        $usuario->save();
+
+        //REdireccionar
+
+        return redirect()->route('posts.index', $usuario->username);
+
+        
     }
 }
